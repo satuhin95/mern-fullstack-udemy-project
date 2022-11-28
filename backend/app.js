c2d8e350e7e8fc841829196e0ce668f2660a9382
satuhin95 <saifcm95@gmail.com>
2022-11-28 09:58:32 +0600

    adding own error Model

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,15 @@ const bodyParser = require('body-parser');
 const placesRoute = require('./routes/placesRoute')
 
 const app = express();
-app.use(placesRoute);
+app.use('/api/places',placesRoute);
+
+app.use((error,req,res,next)=>{
+    if (res.headerSend) {
+        return next(error);
+    }
+    res.status(error.code || 500);
+    res.json({message:error.message || 'An unknown error occurred!'})
+})
 
 
 app.listen(5000,()=>{
--- backend/routes/placesRoute.js
+++ backend/routes/placesRoute.js
@@ -1,10 +1,36 @@
-const express = require('express');
+const express = require("express");
+const HttpError = require("../models/httpError");
 
 const router = express.Router();
+const DUMMY_DATA = [
+  {
+    id: "p1",
+    title: "Empire state Building",
+    description: "One of the most famous sky scrapers in the world!",
+    address: "New York,USA",
+    location: {
+      lat: 40.7484474,
+      lng: -74.9871516,
+    },
+    creator: "u1",
+  },
+];
+router.get("/:pId", (req, res, next) => {
+  const placeId = req.params.pId;
+  const place = DUMMY_DATA.find((item) => item.id === placeId);
+  if (!place) {
+    throw new HttpError("Could not find a place for the provided id", 404);
+  }
+  res.json({ place });
+});
 
-router.get('/',(req,res,next)=>{
-    console.log('Get Request in places');
-    res.json({message:"It's Works!"})
-})
+router.get("/user/:uId", (req, res, next) => {
+  const userId = req.params.uId;
+  const place = DUMMY_DATA.find((item) => item.creator === userId);
+  if (!place) {
+    return next(new HttpError("Could not find a place for the provided id", 404))
+  }
+  res.json({ place });
+});
 
-module.exports =router;
+module.exports = router;
